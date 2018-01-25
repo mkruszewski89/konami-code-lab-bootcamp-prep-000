f92fb1e1b380883d47c9dfac5a39b19b2073929b
Michael Kruszewski <mdkruszewski@gmail.com>
2018-01-25 18:14:55 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,16 +2,18 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 var index = 0
 
 function init() {
-  document.body.addEventListener('keydown', onKeyDownHandler(e) {
-    const key = parseInt(e.which)
-    if (key === code[index]) {
-      index += 1
-      if (index === code.length) {
-        alert("Hurray!")
-        index = 0
-      }
-    } else {
+  document.body.addEventListener('keydown', onKeyDownHandler)
+}
+
+function onKeyDownHandler(e) {
+  const key = parseInt(e.which)
+  if (key === code[index]) {
+    index += 1
+    if (index === code.length) {
+      alert("Hurray!")
       index = 0
     }
-  })
+  } else {
+    index = 0
+  }
 }
